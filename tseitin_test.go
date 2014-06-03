54854d0af6d1000577693c143d95010993ce8024
Simeon Visser <simeon87@gmail.com>
2014-06-04 00:19:12 +0200

    Renamed test functions in error messages as well

--- tseitin_test.go
+++ tseitin_test.go
@@ -6,7 +6,7 @@ import "testing"
 func TestParseFormulaLiteral(t *testing.T) {
 	result := parseFormula("(a)")
 	if result.value != "(a)" {
-		t.Error("Test_parseFormulaLiteral failed: " + result.value)
+		t.Error("TestParseFormulaLiteral failed: " + result.value)
 	}
 	fmt.Println(result.value)
 }
@@ -14,13 +14,13 @@ func TestParseFormulaLiteral(t *testing.T) {
 func TestParseFormulaConjunction(t *testing.T) {
 	result := parseFormula("((a)^(b))")
 	if result.value != "^" {
-		t.Error("Test_parseFormulaConjunction failed for connective: " + result.value)
+		t.Error("TestParseFormulaConjunction failed for connective: " + result.value)
 	}
 	if result.left == nil || result.left.value != "(a)" {
-		t.Error("Test_parseFormulaConjunction failed for left formula.")
+		t.Error("TestParseFormulaConjunction failed for left formula.")
 	}
 	if result.right == nil || result.right.value != "(b)" {
-		t.Error("Test_parseFormulaConjunction failed for right formula.")
+		t.Error("TestParseFormulaConjunction failed for right formula.")
 	}
 	fmt.Println(result.value)
 }
