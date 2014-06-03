3f22ec7b5ca0799749391e3acba320b98d62af42
Simeon Visser <simeon87@gmail.com>
2014-06-03 20:57:09 +0200

    Added initial tests

---
+++ tseitin_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import "testing"
+
+func Test_parseFormulaLiteral(t *testing.T) {
+	result := parseFormula("(a)")
+	if result.value != "(a)" {
+		t.Error("Test_parseFormulaLiteral failed: " + result.value)
+	}
+	fmt.Println(result.value)
+}
+
+func Test_parseFormulaConjunction(t *testing.T) {
+	result := parseFormula("((a)^(b))")
+	if result.value != "^" {
+		t.Error("Test_parseFormulaConjunction failed for connective: " + result.value)
+	}
+	if result.left == nil || result.left.value != "(a)" {
+		t.Error("Test_parseFormulaConjunction failed for left formula.")
+	}
+	if result.right == nil || result.right.value != "(b)" {
+		t.Error("Test_parseFormulaConjunction failed for right formula.")
+	}
+	fmt.Println(result.value)
+}
